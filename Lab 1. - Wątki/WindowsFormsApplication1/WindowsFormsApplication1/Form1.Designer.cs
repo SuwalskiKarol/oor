0355094c2b8fd2cfc19fd3d93b55d23b46bc7a06
Marek Jedlinski <jedlin1054a@gmail.com>
2015-12-01 12:33:33 +0100

    multithreadning

---
+++ Lab 1. - Wątki/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(473, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(144, 248);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(8, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 96);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "F1, F2-zmień prędkość\r\nF3, F4-zmień kierunek\r\n\r\nF5-move\r\nF6-abort\r\nF7-pause\r\nF8-r" +
+    "estart";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.numericUpDown4);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.numericUpDown3);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(309, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(144, 248);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Stwórz nowy wątek";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(8, 64);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 20);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Szybkość:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown4.Location = new System.Drawing.Point(80, 64);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown4.TabIndex = 12;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Black;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Location = new System.Drawing.Point(80, 136);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(8, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 24);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "OK";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Kolor:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(80, 100);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kąt:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Napis:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(8, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(272, 248);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(8, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "X:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown2.Location = new System.Drawing.Point(24, 174);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            248,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown2.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(72, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Y:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown3.Location = new System.Drawing.Point(88, 174);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            272,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown3.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 276);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Multithread);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label5;
+
+    }
+}
+
---
+++ Lab 1. - Wątki/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing.Text;
+using System.Collections;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private readonly object LockObject = new object();   
+        ManualResetEvent _pauseEvent = new ManualResetEvent(false);
+        private readonly string MUTEX_GUID = "969f7729-3867-4583-8bd6-e990d262c94e";
+        private Semaphore semafor = null;
+        private Mutex mutexxx = null;
+ 
+        private ArrayList drawStrings;
+        private DrawString selectedString;
+        private Font normalFont = new Font("Arial", 10);
+        private Font boldFont = new Font("Arial", 11, FontStyle.Bold);
+
+        public Form1()
+        {
+            semafor = new Semaphore(1, 1);
+            InitializeComponent();
+            drawStrings = new ArrayList();
+            Random rnd = new Random();
+            int speed = rnd.Next(10) + 1;
+            int x = rnd.Next(panel1.Width);
+            int y = rnd.Next(panel1.Height);
+            int angle = rnd.Next(360);
+            int red = rnd.Next(256);
+            int green = rnd.Next(256);
+            int blue = rnd.Next(256);
+
+            DrawString ds = new DrawString("pierwszy napis", Color.FromArgb(red, green, blue), angle, x, y, speed, panel1);
+            drawStrings.Add(ds);
+            selectedString = ds;
+
+            ThreadStart startDelegate = new ThreadStart(ds.Start);
+            Thread t = new Thread(startDelegate);
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        [STAThread]
+        static void main()
+        {
+            
+            CheckForIllegalCrossThreadCalls = false;
+            Application.Run(new Form1());
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Graphics dc = e.Graphics;
+            StringFormat format = new StringFormat(StringFormat.GenericTypographic);
+            foreach (DrawString ds in drawStrings)
+            {
+                lock (ds)
+                {
+                    dc.TranslateTransform(ds.X, ds.Y);
+                    dc.RotateTransform(ds.Angle);
+                    dc.DrawString(ds.Text, ds.Selected ? boldFont : normalFont, ds.Brush, 0, 0, format);
+                    dc.ResetTransform();
+                }
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            colorDialog1.Color = label7.BackColor;
+            DialogResult dr = colorDialog1.ShowDialog();
+            if (dr == DialogResult.OK) label7.BackColor = colorDialog1.Color;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                if (textBox1.Text.Trim() == "") return;
+                if (selectedString != null) selectedString.Selected = false;
+                DrawString ds = new DrawString(textBox1.Text, label7.BackColor, (float)numericUpDown1.Value, (float)numericUpDown2.Value, (float)numericUpDown3.Value, (int)numericUpDown4.Value, panel1);
+                selectedString = ds;
+                drawStrings.Add(ds);
+                panel1.Refresh();
+
+            
+            /*try
+            {
+               semafor.WaitOne();*/
+                ThreadStart startDelegate = new ThreadStart(ds.Start);
+                Thread t = new Thread(startDelegate);
+                t.IsBackground = true;
+                t.Start();
+          /* }
+            finally
+            {
+                semafor.Release();
+            }*/
+        }
+
+        
+            
+        
+        private void Multithread(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+
+            mutexxx = new Mutex(false, MUTEX_GUID);      
+
+            if (selectedString == null) return;
+            Keys keyPress = e.KeyCode;
+            switch (keyPress)
+            {
+                case (Keys.F1):
+                    {
+                        lock (selectedString)
+                        {
+                            if (selectedString.Speed < 10) selectedString.Speed++;
+                        }
+                        break;
+                    }
+                case (Keys.F2):
+                    {
+                        lock (selectedString)
+                        {
+                            if (selectedString.Speed > 1) selectedString.Speed--;
+                        }
+                        break;
+                    }
+                case (Keys.F3):
+                    {
+                        Monitor.Enter(LockObject);
+                        try
+                       // lock (selectedString)
+                        {
+                            selectedString.Angle -= 10;
+                            if (selectedString.Angle < 0) selectedString.Angle += 360;
+                        }
+                        finally
+                        {
+                            Monitor.Exit(LockObject);
+                        }
+                        panel1.Refresh();
+                        break;
+                    }
+                case (Keys.F4):
+                    {
+                        mutexxx.WaitOne();
+
+                        {
+                            try
+                            {
+                                selectedString.Angle += 10;
+                                if (selectedString.Angle >= 360) selectedString.Angle -= 360;
+                            }
+                            finally
+                            {
+                                mutexxx.ReleaseMutex();
+
+                            }
+                        }
+                        panel1.Refresh();
+                        break;
+                    }
+                case (Keys.F5):
+                    {
+                        int index = drawStrings.IndexOf(selectedString);
+                        if (++index > drawStrings.Count - 1) index = 0;
+                        lock (selectedString)
+                        {
+                            selectedString.Selected = false;
+                        }
+                        selectedString = (DrawString)drawStrings[index];
+                        lock (selectedString)
+                        {
+                            selectedString.Selected = true;
+                        }
+                        panel1.Refresh();
+                        break;
+                    }
+                case (Keys.F6):
+                    {
+                        int index = drawStrings.IndexOf(selectedString);
+                        drawStrings.Remove(selectedString);
+                        if ((selectedString.Thread.ThreadState & ThreadState.Suspended) == ThreadState.Suspended) selectedString.Thread.Resume();
+                        selectedString.Thread.Abort();
+                        selectedString.Dispose();
+                        if (index > drawStrings.Count - 1) index = 0;
+                        if (drawStrings.Count == 0)
+                            selectedString = null;
+                        else
+                        {
+                            selectedString = (DrawString)drawStrings[index];
+                            selectedString.Selected = true;
+                        }
+                        panel1.Refresh();
+                        break;
+                    }
+                case (Keys.F7):
+                    {
+                        /*_pauseEvent.WaitOne();
+                        if (selectedString.Thread.ThreadState == System.Threading.ThreadState.WaitSleepJoin)
+                        {
+                            _pauseEvent.Set();
+                        }
+
+                        else
+                        {
+                            _pauseEvent.Reset();
+                        } 
+                            
+                              
+                            */
+                        selectedString.Thread.Suspend();
+                        break;
+                    }
+                case (Keys.F8):
+                    {
+                        if ((selectedString.Thread.ThreadState & ThreadState.Suspended) == ThreadState.Suspended)
+                              selectedString.Thread.Resume();
+                           // _pauseEvent.Set();
+                         // wh.WaitOne();
+                        break;
+                    }
+            }
+        }
+
+
+    }
+
+
+    public class DrawString : IDisposable
+    {
+
+        private string text;
+        private float angle;
+        private float x;
+        private float y;
+        private bool selected;
+        private Color colour;
+        private int speed;
+        private SolidBrush brush;
+        private Control ctrl;
+        private Thread thread;
+
+        public DrawString(string text, Color colour, float angle, float x, float y, int speed, Control ctrl)
+        {
+            this.text = text;
+            this.angle = angle;
+            this.x = x;
+            this.y = y;
+            this.colour = colour;
+            this.speed = speed;
+            this.ctrl = ctrl;
+            selected = true;
+            brush = new SolidBrush(colour);
+        }
+
+        public string Text
+        {
+            get { return text; }
+        }
+
+        public float Angle
+        {
+            get { return angle; }
+            set { angle = value; }
+        }
+
+        public float X
+        {
+            get { return x; }
+        }
+
+        public float Y
+        {
+            get { return y; }
+        }
+
+        public Color Colour
+        {
+            get { return colour; }
+        }
+
+        public SolidBrush Brush
+        {
+            get { return brush; }
+        }
+
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public bool Selected
+        {
+            get { return selected; }
+            set { selected = value; }
+        }
+
+        public Thread Thread
+        {
+            get { return thread; }
+        }
+
+        public void Start()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false; 
+            try
+            {
+                thread = Thread.CurrentThread;
+                for (; ; )
+                {
+                    Thread.Sleep(new TimeSpan((11 - speed) * 100000));
+                    lock (this)
+                    {
+                        x += (float)Math.Cos(Math.PI * angle / 180);
+                        y += (float)Math.Sin(Math.PI * angle / 180);
+                        if (x >= ctrl.Width) x = x - ctrl.Width;
+                        if (y >= ctrl.Height) y = y - ctrl.Height;
+                        if (x < 0) x = x + ctrl.Width;
+                        if (y < 0) y = y + ctrl.Height;
+                    }
+                    
+                    ctrl.Refresh();
+                }
+            }
+            catch (ThreadAbortException)
+            {
+            }
+        }
+
+        public void Dispose()
+        {
+            brush.Dispose();
+        }
+    }
+
+
+
+
+
+}
