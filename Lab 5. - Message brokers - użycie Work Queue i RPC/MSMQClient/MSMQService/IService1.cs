26e4bc8a67efc3e775d56e839cbad229ac603b8c
Karol Suwalski <[suwalski.karol@gmail.com]>
2015-12-16 21:56:32 +0100

    message queue

---
+++ Lab 5. - Message brokers - użycie Work Queue i RPC/MSMQClient/MSMQService/IService1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MSMQService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract(IsOneWay = true)]
+        void SendLockMessage(string msg, DateTime sendDate);
+
+        [OperationContract(IsOneWay = true)]
+        void SendToOutputWindow(string msg, DateTime sendDate);
+    }
+}
---
+++ Lab 5. - Message brokers - użycie Work Queue i RPC/MSMQClient/MSMQService/Service1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace MSMQService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public void SendToOutputWindow(string msg, DateTime sendDate)
+        {
+            Debug.WriteLine(msg + " Otrzymana: " + System.DateTime.Now + " Wysłana: " + sendDate);
+        }
+
+        private static readonly object SyncObject = new object();
+        private static readonly TextWriter W = new StreamWriter("log.txt", true);
+
+        public void SendLockMessage(string msg, DateTime sendDate)
+        {
+            lock (SyncObject)
+            {
+                W.WriteLine("Otrzymana: {0} {1}", DateTime.Now.ToLongTimeString(),
+                    DateTime.Now.ToLongDateString());
+                W.WriteLine("Wysłana {0} {1}", sendDate.ToLongTimeString(),
+                    sendDate.ToLongDateString());
+                W.WriteLine(" :");
+                W.WriteLine(" :{0}", msg);
+                W.WriteLine("-------------------------------");
+                W.Flush();
+            }
+        }
+
+
+
+    }
+}
