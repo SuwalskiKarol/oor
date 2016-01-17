aa29482d7ea147b99c6dfe8b07a9c4265d9435fc
Karol Suwalski <[suwalski.karol@gmail.com]>
2016-01-17 13:59:02 +0100

    process

---
+++ Lab 2. - Równoległość procesów/plinq/add/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] kolekcjaLiczb = new[] { 1, 4, 6, 5, 3, 2, 8, 9, 0 };
+
+            var querry1 = kolekcjaLiczb.Select(n => n).ToArray().OrderBy(b=>b);
+
+            foreach (var item in querry1)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab 2. - Równoległość procesów/plinq/example/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace example
+{
+    class Program
+    {
+        private static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.UtcNow;
+
+            IEnumerable<int> items = Enumerable.Range(0, 20);
+
+            var results = items
+                .Select(ProcessItem)
+                .ToList();
+
+            results.ForEach(Console.WriteLine);
+
+            DateTime end = DateTime.UtcNow;
+            TimeSpan duration = end - start;
+
+            Console.WriteLine("Finished. Took {0}", duration);
+
+            Console.ReadLine();
+        }
+
+        private static string ProcessItem(int item)
+        {
+            int pause = rnd.Next(900, 1100);
+            Thread.Sleep(pause);
+
+            return string.Format("Result of item {0}", item);
+        }
+    }
+}
